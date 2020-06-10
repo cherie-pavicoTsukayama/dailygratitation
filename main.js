76da47b3223bb27ea69a960ce5090477ad99d132
Cherie <cherietsukayama@gmail.com>
2020-06-10 19:02:27 +0000

    timer counts down from 1:59 mintues when the user  presses the start button

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const startButton = document.getElementById('start');
 const pauseButton = document.getElementById('pause');
+const secondsText = document.getElementById('secondsText');
+const minutesText = document.getElementById('minutesText')
 // eslint-disable-next-line no-unused-vars
 let startCounterId = null;
 // eslint-disable-next-line no-unused-vars
-let counter = 30;
+let seconds = '59';
+let minutes = 1;
 
 startButton.addEventListener('click', hideStartButton);
 pauseButton.addEventListener('click', hidePauseButton);
+
 
 function hideStartButton() {
   startButton.classList.add('display-none');
@@ -25,11 +29,32 @@ function handleClickStartTime() {
 }
 
 function startCounter() {
-  if (counter > 0) {
-    counter -= 1;
-    console.log(counter)
+
+  if (seconds > 0) {
+    seconds -= 1;
+    secondsText.textContent = seconds;
   }
 
+  if (seconds === 59) {
+    minutesText.textContent = minutes;
+    minutes -= 1
+  }
+
+  if (minutes === 1 && seconds === 0) {
+    seconds = 60;
+    minutes = 0;
+    minutesText.textContent = minutes;
+  }
+
+  if (minutes === 0 && seconds === 0) {
+    clearInterval(startCounterId);
+    startButton.classList.remove('display-none');
+    pauseButton.classList.add('display-none');
+    seconds = '59';
+    minutes = 1;
+    minutesText.textContent = minutes;
+    secondsText.textContent = seconds;
+  }
 }
 
 function handleClickPauseTime() {
